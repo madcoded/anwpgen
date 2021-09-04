7665589840bf04b0fd4ca959bd0a0232ad84f29c
madcoded <aocfreeman@gmail.com>
2021-09-04 11:13:05 +0530

    modified functions.php and added screenshot.png

--- functions.php
+++ functions.php
@@ -6,12 +6,13 @@
  */
 
  function anwpgen_enqueue_script (){
-     wp_enqueue_style( 'style-css', get_stylesheet_uri(), [], filemtime(get_template_directory( ). '/style.css'), 'all');
-     wp_enqueue_script( 'main-js', get_template_directory_uri( ). '/assets/main.js', [], filemtime(get_template_directory(  ). '/assets/main.js'), true );
-
-     /* regsiter style sheet not enqueue it right away. may be use on particular page */
-     //wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime(get_template_directory( ). '/style.css'), 'all');
-     //wp_enqueue('style.css');
+     /* registering style and script*/
+    wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime(get_template_directory( ). '/style.css'), 'all');
+    wp_register_script( 'main-js', get_template_directory_uri( ). '/assets/main.js', [], filemtime(get_template_directory(  ). '/assets/main.js'), true );
+   
+    /* calling style and script*/
+    wp_enqueue_style( 'style-css');
+    wp_enqueue_script( 'main-js');
      
  }
  add_action ('wp_enqueue_scripts', 'anwpgen_enqueue_script');
