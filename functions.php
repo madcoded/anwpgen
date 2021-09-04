770ee91b0f9e426fec763e07abbd182fafc937b2
madcoded <aocfreeman@gmail.com>
2021-09-04 11:07:03 +0530

    modified functions.php and added assets folder

--- functions.php
+++ functions.php
@@ -6,6 +6,12 @@
  */
 
  function anwpgen_enqueue_script (){
-     wp_enqueue_style( 'stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory( ). '/style.css'), 'all');
+     wp_enqueue_style( 'style-css', get_stylesheet_uri(), [], filemtime(get_template_directory( ). '/style.css'), 'all');
+     wp_enqueue_script( 'main-js', get_template_directory_uri( ). '/assets/main.js', [], filemtime(get_template_directory(  ). '/assets/main.js'), true );
+
+     /* regsiter style sheet not enqueue it right away. may be use on particular page */
+     //wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime(get_template_directory( ). '/style.css'), 'all');
+     //wp_enqueue('style.css');
+     
  }
  add_action ('wp_enqueue_scripts', 'anwpgen_enqueue_script');
