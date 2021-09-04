7d4a5416144d5b89c55a67f215974122b16d0634
madcoded <aocfreeman@gmail.com>
2021-09-04 10:53:43 +0530

    modified functions.php

--- functions.php
+++ functions.php
@@ -4,3 +4,8 @@
  *
  * @package Anwpgen
  */
+
+ function anwpgen_enqueue_script (){
+     wp_enqueue_style( 'stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory( ). '/style.css'), 'all');
+ }
+ add_action ('wp_enqueue_scripts', 'anwpgen_enqueue_script');
